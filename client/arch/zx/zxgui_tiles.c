cd45d6816371328e3502cb00795de5acd4c27890
syroegkin <syroegkin@gmail.com>
2026-04-19 11:47:07 +0200

    fix: fix tiles mess

--- client/arch/zx/zxgui_tiles.c
+++ client/arch/zx/zxgui_tiles.c
@@ -2,8 +2,11 @@
 #include <stdint.h>
 typedef uint8_t uchar;
 
+#define tiles gui_tiles_data
+#include "tiles.h"
+#undef tiles
+
 uint8_t* get_gui_tiles()
 {
-#   include "tiles.h"
-    return tiles;
+    return gui_tiles_data;
 }
